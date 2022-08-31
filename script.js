68d2f31655f85280ebe0b4b6d02a2fbb226e325c
uelionobre <uelionobre@hotmail.com>
2022-08-31 19:02:27 -0300

    Gera cores aleatoriaas

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+const cores = ['black', 'red', 'green', 'blue'];
 function requisito2() {
-  const cores = ['black', 'red', 'green', 'blue'];
   const divColors = document.querySelectorAll('.color');
   for (let index = 0; index < divColors.length; index += 1) {
     divColors[index].style.backgroundColor = cores[index];
   }
 }
 
-// function requisito3() {}
+// Gera cores aleatorias
+function requisito3() {
+  for (let index = 1; index < cores.length; index += 1) {
+    const corAleatoria = `#${Math.random().toString(16).substring(2, 8)}`;
+    cores[index] = corAleatoria;
+  }
+  requisito2();
+}
+
+function eventsListeners() {
+  const buttonRandomColor = document.getElementById('button-random-color');
+  buttonRandomColor.addEventListener('click', requisito3);
+}
 
 window.onload = () => {
   requisito2();
-  // requisito3();
+  eventsListeners();
 };
