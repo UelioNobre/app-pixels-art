e9189e8b2445452e7502bf0745ee16c083fe90f5
uelionobre <uelionobre@hotmail.com>
2022-08-31 18:53:10 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function requisito2() {
+  const cores = ['black', 'red', 'green', 'blue'];
+  const divColors = document.querySelectorAll('.color');
+  for (let index = 0; index < divColors.length; index += 1) {
+    divColors[index].style.backgroundColor = cores[index];
+  }
+}
 
+// function requisito3() {}
+
+window.onload = () => {
+  requisito2();
+  // requisito3();
+};
