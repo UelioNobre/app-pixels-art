fcd034fb2d62dc67311e9668c4cfd84b25f002f8
uelionobre <uelionobre@hotmail.com>
2022-08-31 17:03:02 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-function createRandomColor() {
-  return `#${Math.random().toString(16).substring(2, 8)}`;
+function createPalette() {
+  const colors = [];
+  const size = document.querySelectorAll('.color').length;
+
+  for (let index = 0; index < size - 1; index += 1) {
+    const color = `#${Math.random().toString(16).substring(2, 8)}`;
+    colors.push(color);
+  }
+
+  localStorage.setItem('colorPalette', JSON.stringify(colors));
 }
 
 function addColorToDom() {
   const domColors = document.querySelectorAll('.color');
   domColors[0].style.backgroundColor = 'black';
-  for (let index = 1; index < domColors.length; index += 1) {
-    domColors[index].style.backgroundColor = `${createRandomColor()}`;
+
+  if (!localStorage.getItem('colorPalette')) {
+    createPalette();
   }
+
+  const colors = JSON.parse(localStorage.getItem('colorPalette'));
+  for (let index = 0; index < domColors.length - 1; index += 1) {
+    domColors[index + 1].style.backgroundColor = colors[index];
+  }
+}
+
+function createNewPalette() {
+  localStorage.removeItem('colorPalette');
+  addColorToDom();
 }
 
 window.onload = () => {
   addColorToDom();
 
   const btnGenerateRandomColors = document.getElementById('button-random-color');
-  btnGenerateRandomColors.addEventListener('click', addColorToDom);
+  btnGenerateRandomColors.addEventListener('click', createNewPalette);
 };
