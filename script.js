3f6a07d584048c55342f6c3dc2dd1a6f99c42410
uelionobre <uelionobre@hotmail.com>
2022-08-31 20:27:08 -0300

    Salva a paleta de cores no localStorage

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const cores = ['black', 'red', 'green', 'blue'];
+let cores = ['black', 'red', 'green', 'blue'];
+
+// Requisito 5
+function saveColors() {
+  localStorage.setItem('colorPalette', JSON.stringify(cores));
+}
+function getColors() {
+  if (!localStorage.getItem('colorPalette')) {
+    saveColors();
+  }
+
+  cores = JSON.parse(localStorage.getItem('colorPalette'));
+  return cores;
+}
+
+// Adiciona cores à paleta
 function requisito2() {
   const divColors = document.querySelectorAll('.color');
+  const colors = getColors();
   for (let index = 0; index < divColors.length; index += 1) {
-    divColors[index].style.backgroundColor = cores[index];
+    divColors[index].style.backgroundColor = colors[index];
   }
 }
 
@@ -12,15 +28,18 @@ function requisito3() {
     const corAleatoria = `#${Math.random().toString(16).substring(2, 8)}`;
     cores[index] = corAleatoria;
   }
+  saveColors();
   requisito2();
 }
 
 function eventsListeners() {
   const buttonRandomColor = document.getElementById('button-random-color');
   buttonRandomColor.addEventListener('click', requisito3);
+  buttonRandomColor.addEventListener('click', saveColors);
 }
 
 window.onload = () => {
   requisito2();
   eventsListeners();
+  getColors();
 };
